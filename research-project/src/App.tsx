193ff07286a0ffbdfecc807f5dd43a2fd868b2f7
Jason <jstarace@mac.com>
2023-12-17 20:31:09 -0800

    Update App.tsx

--- research-project/src/App.tsx
+++ research-project/src/App.tsx
@@ -2,12 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavigationBar from "./components/NavigationBar";
 
 function App() {
-  let items = ["New York", "San Francisco", "Rome", "Amsterdam", "Munich"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <>
       <NavigationBar></NavigationBar>
