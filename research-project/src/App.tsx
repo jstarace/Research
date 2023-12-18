d7f4739d0d75f57e6b3ec24f26c21ce74e048960
Jason <jstarace@mac.com>
2023-12-17 16:00:30 -0800

    Initial Commit
    before to far

---
+++ research-project/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import ListGroup from "./components/ListGroup";
+
+import "./App.css";
+
+function App() {
+  let items = ["New York", "San Francisco", "Rome", "Amsterdam", "Munich"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
